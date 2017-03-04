9d0166edb5a396c0826ce6d42700669116eb8dc9
Jay Stevens <jay@mandarincreativegroup.com>
2017-03-04 14:30:26 +0000

    Add logging and conditional args to  commandline script

--- genapi.sh
+++ genapi.sh
@@ -1,16 +1,42 @@
+build='n'
+push='n'
+
+while [[ "$#" > 1 ]]; do case $1 in
+    --build) build='y';;
+    --push) push='y';;
+    *) break;;
+  esac; shift; shift
+done
+
+echo -e "\e[34m --> Pull current CLS-Core...\e[0m"
 cd ~/cls-core/
 git pull
+echo -e "\e[34m --> Pull current codegen...\e[0m"
 cd ~/swagger-codegen/
 git pull
-#./swaggerdocker.sh mvn clean package
+
+if [ $build == 'y' ]
+then 
+    echo -e "\e[34m --> Rebuilding Swagger-Codegen Packages...\e[0m"
+    ./swaggerdocker.sh mvn clean package
+fi
+
+echo -e "\e[34m --> Copy Swagger spec...\e[0m"
 export TEMPLATES=/gen/modules/swagger-codegen/src/main/resources/mcgnetcore
 export SWAGGERFILE=../cls-core/cls-core-swagger.yaml
 cp -f $SWAGGERFILE ./swagger.yaml
+
+echo -e "\e[34m --> Generate API code...\e[0m"
 ./swaggerdocker.sh generate -i swagger.yaml -l aspnetcore -t $TEMPLATES -o /gen/mcgnetcore -c ./config.json
-#cp ./mcgnetcore/src/IO.Swagger/Models/* ~/cls-core/cls-model/Models/api/
-#cp ./mcgnetcore/src/IO.Swagger/Controllers/* ~/cls-core/cls-core/Controllers/api/
-#cd ~/cls-core
-#export DT=`date +%Y%m%d-%H%M%S`
-#git add -A
-#git commit -m "Add/Update API files from Swagger specification as of $DT"
-#git push
+
+if [ $push == 'y' ]
+then 
+    echo -e "\e[34m --> Pushing API changes to git repo...\e[0m"
+    cp ./mcgnetcore/src/IO.Swagger/Models/* ~/cls-core/cls-model/Models/api/
+    cp ./mcgnetcore/src/IO.Swagger/Controllers/* ~/cls-core/cls-core/Controllers/api/
+    cd ~/cls-core
+    export DT=`date +%Y%m%d-%H%M%S`
+    git add -A
+    git commit -m "Add/Update API files from Swagger specification as of $DT"
+    git push
+fi
